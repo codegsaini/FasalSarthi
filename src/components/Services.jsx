3f84294447fc0bc7f7438e9ed2abd0fc19d88da1
Gaurav Saini <codegsaini@gmail.com>
2023-03-21 05:03:18 +0530

    Added service upload section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,6 +4,11 @@ import ServiceItem from "./ServiceItem";
 const Services = () => {
 	return (
 		<div id="service-container">
+			<div id="service-upload-section">
+				<h3>You provide agriculture related service?</h3>
+				<p>List your service at our site today!</p>
+				<button>Submit Service</button>
+			</div>
 			<h2 className="section-label">Affordable services</h2>
 			<div id="services-wrapper">
 				<ServiceItem />
