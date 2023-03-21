c3107cc87ecf3dec9dd51ddf25688bfbfb407f6a
Gaurav Saini <codegsaini@gmail.com>
2023-03-21 07:43:56 +0530

    update-5

--- src/components/ManageFarm.jsx
+++ src/components/ManageFarm.jsx
@@ -3,7 +3,7 @@ import "../style/css/ManageFarm.css";
 import Logo from "../img/crops.png";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 const ManageFarm = () => {
 	const navigate = useNavigate();
 	const { user } = useSelector((state) => state.auth);
@@ -19,6 +19,12 @@ const ManageFarm = () => {
 				<div id="user-info-wrapper">
 					<div id="user-img"></div>
 					<p id="username">Gaurav Saini</p>
+				</div>
+				<div id="admin-nav-links-wrapper">
+					<a href="#">Dashboard</a>
+					<a href="#">My Crops</a>
+					<a href="#">Notification</a>
+					<a href="#">Settings</a>
 				</div>
 			</div>
 		</div>
